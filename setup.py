056d15cb680264818225942a91070eb23ae78c0d
Tran Anh Khoa <khoa102@gmail.com>
2016-08-21 23:05:17 -0500

    Create a MANIFEST.in and a README.rst file
    create those two file and include some update to setup.

--- setup.py
+++ setup.py
@@ -1,11 +1,18 @@
 from setuptools import setup
 
 setup(name='kissanime_downloader',
-      version='0.1',
+      version='0.2',
       description='A program to download anime episode from Kissanime',
       url='http://github.com/khoa102/kissanime_downloader',
       author='Khoa102',
       author_email='khoa102@gmail.com',
       license='MIT',
       packages=['kissanime_downloader'],
-      zip_safe=False)
+      install_requires=[
+          'lxml>=3.6.0 ',
+          'cfscrape>=1.6.6',
+          'requests>=2.11.1',
+      ],
+      zip_safe=False,
+      include_pakage_data=True
+)
